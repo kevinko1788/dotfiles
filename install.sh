bd45d5fed3f0d6dd7c8f65dd84a36dfb572d2453
Kevin Ko <kevinko1788@gmail.com>
2020-10-06 14:13:56 -0700

    add macos brew install in install.sh file

--- install.sh
+++ install.sh
@@ -5,8 +5,13 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt-get install git
     git config --global core.editor "vim"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "OS = MacOS" 
+    echo "OS = MacOS"
+    #install Brew
+    echo "Brew installing"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    echo "Brew install finished"
 fi
+echo "Linking dotfiles"
 ln -sf dotfiles/vim/.vimrc ~/.vimrc
 ln -sf dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf dotfiles/zsh/.zshenv ~/.zshenv
