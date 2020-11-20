5a389a999fe1a1482dfe6876d5730cec8a7c8952
Kevin Ko <kevinko1788@gmail.com>
2020-11-20 09:58:15 -0800

    added tmux instructions

--- install.sh
+++ install.sh
@@ -18,3 +18,5 @@ echo "Linking dotfiles"
 ln -sf dotfiles/vim/.vimrc ~/.vimrc
 ln -sf dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf dotfiles/zsh/.zshenv ~/.zshenv
+ln -sf dotfiles/tmux/.tmux.conf ~/.tmux.conf
+
